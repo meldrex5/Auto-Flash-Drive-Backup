df606482ea3b774a157e87c7393c9c57a3939f90
meldrex5 <btmextra@gmail.com>
2016-09-05 23:52:15 -0400

    Update VDF / Settings
    Removed the account name check.  Fixed the auto VDF so it actually stops
    the program if you tell it not to create the VDF.

--- Flash Drive Backup.bat
+++ Flash Drive Backup.bat
@@ -2,27 +2,28 @@
 
 :: Version 1.4
 :: Created by Brendan Murphy 
+:: To download the newest version of this tool go to https://github.com/meldrex5/Auto-Flash-Drive-Backup/archive/master.zip
 ::
 :: INSTRUCTIONS
 ::
-:: I. To use this program you must create a .txt file called "DriveValidation.txt" in the root 
-::	directory of the drive.
+:: I. To use this program you must allow it to create "DriveValidation.txt" in the root on the
+::	the drive you want to back up.
+::	
 ::
-:: II. When you are given this program it will likely not work the first time.  This is
-::	because you need to set the "User Variables" found below.  All ARE case sensitive.
-::		a) myAccountName: The name of your account, program will only work on this account.
+:: II. When you are given this program it will likely not work the first time. This is
+::	because you need to change the "User Variables" found below. All are case sensitive.
+::
+::		a) myComputerName: The name of your computer, program will only work on this account.
+::			To find, right click on "This PC" in File Explorer and click on "Properties".
+::				THIS MUST BE IN ALL CAPS 
 
-::		b) myComputerName: The name of your computer, program will only work on this account.
-::			To find, right click on "This PC" in File Explorer and click on "Properties"
-::				THIS MUST BE CAPILALIZED
-
-::		c) myDriveLetter: The letter assigned to the drive you want to back up.
+::		b) myDriveLetter: The letter assigned to the drive you want to back up.
 ::			To find, look at the letter next to the drive name in File Explorer.
 ::			(Omit everything except the letter itself)
 ::
 :: III. Files will be backed up according to date and time, to the following directory:
 ::
-::		"C:\Users\USERNAME\Documents\Flash Drive Auto Backups\DATE-TIME"
+::		"C:\Users\USERNAME\Documents\Flash Drive Auto Backups\"DATE-TIME""
 ::
 :: IV. If you decide to stop using this program, your backups will be left untouched and in exactly 
 ::	the same place.
@@ -30,7 +31,7 @@
 ::
 :: Additional Info: This program assumes you will typically make no more than one backup per day.
 ::	If this is not the case, the program will work fine however it will be somewhat difficult
-::	to locate a specific backup after a long time.  If you need this program modified to better
+::	to locate a specific backup after a long time. If you need this program modified to better
 ::	suit your needs (or just need help using it), contact Brendan Murphy at:
 ::
 ::		btm5457@rit.edu OR btmextra@gmail.com 
@@ -43,7 +44,6 @@
 
 ::User Variables
 
-set myAccountName= btm71
 set myComputerName= ULTRAXPS
 set myDriveLetter= d
 
@@ -51,33 +51,38 @@ set myDriveLetter= d
 
 :PROGRAM
 
-::This section will create (or overwrite) the root drive validation file.  
+:Pre-Run
 
+if not exist %myDriveLetter%:\ echo The selected drive is not attached.  Change to another by editing this file. & pause & goto end
+
+::This section will create the root drive validation file.  
 
 if exist %myDriveLetter%:\DriveValidation.txt goto Device Validation
-echo Validation File not found.  Would you like to create it on drive %myDriveLetter%?
+echo Validation File not found. Would you like to create it on drive %myDriveLetter%?
 choice
-if errorlevel==0 echo This file allows the autobackup script on %myComputerName% to function.  To disable, rename this file. >%myDriveLetter%:\DriveValidation.txt & cls & goto Device Validation 
-if errorlevel==1 goto end
+if errorlevel==2 echo You have chosen not to validate your drive. Re-run program if this was a mistake & pasue & goto end
+if errorlevel==1 echo This file allows the autobackup script on %myComputerName% to function. To disable, rename this file. >%myDriveLetter%:\DriveValidation.txt & cls & goto Device Validation 
+
 
 
 
 echo Open this file in Notepad to read Instructions and Change Settings
+
 :Device Validation
+
 if %computername% == %myComputerName% echo Computer Validation: PASSED
 if not %computername% == %myComputerName% echo Computer Validation: FAILED & goto failure 
-
-if %username% == %myAccountName% echo Account Validation: PASSED
-if not %username% == %myAccountName% echo Account Validation: FAILED & goto failure 
-
 
 if exist %myDriveLetter%:\DriveValidation.txt echo Drive Validation: PASSED
 if not exist %myDriveLetter%:\DriveValidation.txt echo Drive Validation: FAILED & goto failure
 
+echo Welcome %USERNAME%!
+
 
 :backup
 
-::Date/Time Entry
+:Date/Time Entry
+
 echo ----------
 echo Enter Date and Millitary Time in the following format: MM-DD-YY---HH-MM
 echo If you enter it wrong, you'll have to deal with it later.
@@ -85,7 +90,7 @@ set /p datetime=
 
 
 ::This section creates the directory to backup to based on current date and 
-::  time THEN copys files from drive to the new directory.
+::	time THEN copys files from drive to the new directory.
 
 mkdir C:\Users\%username%\Documents\"Flash Drive Auto Backups"\%datetime%
 
@@ -97,8 +102,9 @@ goto end
 
 
 :failure
+
 echo ----------
-echo There was a failure somwhere in the backup process.  Ensure settings are correct, drive is inserted and try again.
+echo There was a failure somwhere in the backup process. Ensure settings are correct, drive is inserted and try again.
 pause
 
 :end
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Auto-Flash-Drive-Backup
 A Windows Batch tool that automagically backs up a drive to a specific location.
+
+Version 1.4
+:: Created by Brendan Murphy 
+:: To download the newest version of this tool go to https://github.com/meldrex5/Auto-Flash-Drive-Backup/archive/master.zip
+::
+:: INSTRUCTIONS
+::
+:: I. To use this program you must allow it to create "DriveValidation.txt" in the root on the
+::	the drive you want to back up.
+::	
+::
+:: II. When you are given this program it will likely not work the first time. This is
+::	because you need to change the "User Variables" found in the program file. All are case sensitive.
+::
+::		a) myComputerName: The name of your computer, program will only work on this account.
+::			To find, right click on "This PC" in File Explorer and click on "Properties".
+::				THIS MUST BE IN ALL CAPS 
+
+::		b) myDriveLetter: The letter assigned to the drive you want to back up.
+::			To find, look at the letter next to the drive name in File Explorer.
+::			(Omit everything except the letter itself)
+::
+:: III. Files will be backed up according to date and time, to the following directory:
+::
+::		"C:\Users\USERNAME\Documents\Flash Drive Auto Backups\"DATE-TIME""
+::
+:: IV. If you decide to stop using this program, your backups will be left untouched and in exactly 
+::	the same place.
+::
+::
+:: Additional Info: This program assumes you will typically make no more than one backup per day.
+::	If this is not the case, the program will work fine however it will be somewhat difficult
+::	to locate a specific backup after a long time. If you need this program modified to better
+::	suit your needs (or just need help using it), contact Brendan Murphy at:
+::
+::		btm5457@rit.edu OR btmextra@gmail.com 
+::	
+::	This program can be modified by the end user however it WILL stop working if you change something
+:: 	and don't know what you're doing.  
